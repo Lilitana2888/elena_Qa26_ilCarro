134fa844e251af52349de38b5e1aea6992300aa1
Lilitana2888 <76058199+Lilitana2888@users.noreply.github.com>
2021-01-09 20:50:00 +0200

    testng.xml

--- src/test/java/com/examle/ilcarro/qa/tests/TestBase.java
+++ src/test/java/com/examle/ilcarro/qa/tests/TestBase.java
@@ -1,19 +1,24 @@
 package com.examle.ilcarro.qa.tests;
 
 import com.examle.ilcarro.qa.application.ApplicationManager;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.remote.BrowserType;
+
 
 public class TestBase {
 
-    protected ApplicationManager app = new ApplicationManager();
+ //   protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-    @BeforeClass
+    @BeforeSuite
     public void setUp(){
         app.start();
     }
 
-    @AfterMethod()
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
